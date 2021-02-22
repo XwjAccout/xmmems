d9b63bedeed6c91ad8b03431e0de7a6749c715a7
admin <java2017hxw@163.com>
2021-02-22 16:58:09 +0800

    2021-02-22  1、删除用户角色中间表相关实体类，mapper，xml 2、查找父菜单Pmenu 时查询用户角色id是集合

--- src/main/java/com/xmmems/service/system/MenuService.java
+++ src/main/java/com/xmmems/service/system/MenuService.java
@@ -6,12 +6,10 @@ import com.xmmems.common.auth.domain.UserHolder;
 import com.xmmems.common.exception.ExceptionEnum;
 import com.xmmems.common.exception.XMException;
 import com.xmmems.common.utils.BeanHelper;
-import com.xmmems.domain.account.AccountRoleKey;
 import com.xmmems.domain.role.Menu;
 import com.xmmems.domain.role.MenuExample;
 import com.xmmems.dto.MenuDTO;
 import com.xmmems.mapper.MenuMapper;
-import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,7 +23,6 @@ import java.util.Map;
 
 @Service
 @Transactional
-@Slf4j
 public class MenuService {
 
     @Autowired
@@ -106,53 +103,69 @@ public class MenuService {
 
     /**
      * 查询所有一级菜单
+     *
      * @return
      */
     public List<MenuDTO> findPmenu() {
-        MenuExample example = new MenuExample();
         //查询当前用户的角色
-        AccountRoleKey account = menuMapper.roleByAccoutId(UserHolder.loginId());
-        List<Menu> menus = menuMapper.roleById(account.getRoleId());
-        if (CollectionUtils.isEmpty(menus)) {
+        List<Map<Integer, Integer>> account_roles = menuMapper.roleByAccoutId(UserHolder.loginId());
+        if (account_roles.size() == 0) {
+            throw new XMException(ExceptionEnum.ROLE_NOT_FOUND);
+        }
+        List<Integer> roleids = new ArrayList<>();
+        for (Map<Integer, Integer> account_role : account_roles) {
+            roleids.add(account_role.get("roleId"));
+        }
+        List<Menu> menus = menuMapper.selectMenusByRoleIds(roleids);
+        if (menus.size() == 0) {
             throw new XMException(ExceptionEnum.MENU_NOT_FOUND);
         }
         return BeanHelper.copyWithCollection(menus, MenuDTO.class);
     }
 
     public List<Map<String, Object>> findByPid(String pcode) {
+        //查询当前用户的角色
+        List<Map<Integer, Integer>> account_roles = menuMapper.roleByAccoutId(UserHolder.loginId());
+        if (account_roles.size() == 0) {
+            throw new XMException(ExceptionEnum.ROLE_NOT_FOUND);
+        }
+        List<Integer> roleids = new ArrayList<>();
+        for (Map<Integer, Integer> account_role : account_roles) {
+            roleids.add(account_role.get("roleId"));
+        }
+        //
+        List<Menu> menus = menuMapper.findByPidRoleids(pcode, roleids);
+
         List<Map<String, Object>> list0 = new ArrayList<>();
-        AccountRoleKey account = menuMapper.roleByAccoutId(UserHolder.loginId());
-        List<Menu> menus = menuMapper.findByPid(pcode,account.getRoleId());
-
         //不为空
-        if (!CollectionUtils.isEmpty(menus)) {
-                Map<String, Object> map0 = new HashMap<>();
-                    List<Map<String, Object>> list1 = new ArrayList<>();
-                    for (Menu menu1 : menus) {
-                        if (menu1.getPcode().equals(pcode)) {
-                            Map<String, Object> map1 = new HashMap<>();
-                            map1.put("name", menu1.getName());
-                            map1.put("path", menu1.getNewUrl());
-                            map1.put("id", menu1.getId());
-                            map1.put("icon", menu1.getIcon());
-                            List<Map<String, Object>> list2 = new ArrayList<>();
-                            List<Menu> menuslist = menuMapper.findByPid(menu1.getCode(),account.getRoleId());
-                            for (Menu menu2 : menuslist) {
-                                if (menu2.getPcode().equals(menu1.getCode())) {
-                                    Map<String, Object> map2 = new HashMap<>();
-                                    map2.put("name", menu2.getName());
-                                    map2.put("path", menu2.getNewUrl());
-                                    map2.put("id", menu2.getId());
-                                    map2.put("icon", menu2.getIcon());
-                                    list2.add(map2);
-                                }
+        if (menus.size() > 0) {
+            Map<String, Object> map0 = new HashMap<>();
+            List<Map<String, Object>> list = new ArrayList<>();
+            for (Menu menu : menus) {
+                //                if (menu.getPcode().equals(pcode)) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("name", menu.getName());
+                map.put("path", menu.getNewUrl());
+                map.put("id", menu.getId());
+                map.put("icon", menu.getIcon());
+                List<Map<String, Object>> list2 = new ArrayList<>();
+                List<Menu> menuslist = menuMapper.findByPidRoleids(menu.getCode(), roleids);
+                for (Menu menu2 : menuslist) {
+                    //                        if (menu2.getPcode().equals(menu.getCode())) {
+                    Map<String, Object> map2 = new HashMap<>();
+                    map2.put("name", menu2.getName());
+                    map2.put("path", menu2.getNewUrl());
+                    map2.put("id", menu2.getId());
+                    map2.put("icon", menu2.getIcon());
+                    list2.add(map2);
+                    //                        }
 
-                            }
-                            map1.put("menu", list2);
-                            list1.add(map1);
-                        }
-                    }
-            map0.put("menu", list1);
+                }
+                map.put("menu", list2);
+                list.add(map);
+                //                }
+            }
+            map0.put("menu", list);
             //封装数据
             list0.add(map0);
 
@@ -183,8 +196,8 @@ public class MenuService {
         //传送的对象
         List<Map<String, Object>> list0 = new ArrayList<>();
 
-         List<Menu> menus  = null;
-        if (roleId==null) {
+        List<Menu> menus = null;
+        if (roleId == null) {
             menus = menuMapper.selectAll();
         } else {
             menus = menuMapper.selectByRoleId(roleId);
